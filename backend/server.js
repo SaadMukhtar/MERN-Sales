65917668cb4ef2bc30771c7dfea8d272a109773f
Saad Mukhtar <saadmukhtar01@gmail.com>
2021-08-12 10:55:39 -0400

    Connected MongoDB and modelled data

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
-const express = require("express");
-const products = require("./data/products");
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import products from "./data/products.js";
+
+dotenv.config();
+
+connectDB();
 
 const app = express();
-
-app.listen(5000, console.log("Server running on port 5000"));
 
 app.get("/", (req, res) => {
   res.send("API is running...");
@@ -17,3 +21,10 @@ app.get("/api/products/:id", (req, res) => {
   const product = products.find((p) => p._id === req.params.id);
   res.json(product);
 });
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
