af105817e0190e9b9d42f58eaecdf103d7844ed9
Saad Mukhtar <saadmukhtar01@gmail.com>
2021-09-01 18:09:58 -0400

    Morgab & Create Review Endpoint

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -14,6 +14,11 @@ const reviewSchemna = mongoose.Schema(
       type: String,
       required: true,
     },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
   },
   {
     timestamps: true,
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import path from "path";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import products from "./data/products.js";
+import morgan from "morgan";
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
@@ -15,6 +16,9 @@ connectDB();
 
 const app = express();
 
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 app.use(express.json());
 
 app.get("/", (req, res) => {
